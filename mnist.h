f3f638e572c911af3b6cb2ab88d6b32e7d2972ed
Rohinthan <rokinthanp03@gmail.com>
2026-06-24 13:54:44 +0530

    Update mnist.h

--- mnist.h
+++ mnist.h
@@ -9,10 +9,10 @@
  * Loads the original MNIST binary IDX format files.
  *
  * Download dataset (4 files) into a mnist/ folder:
- *   wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
- *   wget http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
- *   wget http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
- *   wget http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
+ *   wget https://ossci-datasets.s3.amazonaws.com/mnist/train-images-idx3-ubyte.gz
+     wget https://ossci-datasets.s3.amazonaws.com/mnist/train-labels-idx1-ubyte.gz
+     wget https://ossci-datasets.s3.amazonaws.com/mnist/t10k-images-idx3-ubyte.gz
+     wget https://ossci-datasets.s3.amazonaws.com/mnist/t10k-labels-idx1-ubyte.gz
  *   gunzip mnist/*.gz
  *
  * File format:
